966093cf3844a74d7d9a82a42e773b130d7e6997
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-04-22 17:36:27 +0200

    Feat: add search screen

--- src/screens/search/search.screen.js
+++ src/screens/search/search.screen.js
@@ -1,9 +1,34 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import { useForm } from '../../hooks/useForm';
 
 const SearchScreen = props => {
+
+  const initialForm = {
+    searchText: '',
+  };
+  const [ formValues, handleInputChange ] = useForm(initialForm);
+  const { searchText } = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+  }
+
   return (
-    <div>SearchScreen</div>
+    <>
+      <h1>Search hero</h1>
+      <hr></hr>
+      <div className='row'>
+        <div className='col-5'>
+          <h4>Search</h4>
+          <form onSubmit={ handleSearch }>
+            {/* The name have to be the same than value */}
+            <input autoComplete='off' className='form-control' name='searchText' placeholder='Buscar un héroe' type="text" value={ searchText } onChange={ handleInputChange }></input>
+            <button className='btn btn-outline-primary mt-3' type="submit">Buscar</button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
